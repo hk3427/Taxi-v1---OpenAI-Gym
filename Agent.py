9adb05685049af1d268ce5c04e4bc37d2c47e83c
Himanshu Kumar <himanshukumar_dr@srmuniv.edu.in>
2018-04-03 02:38:45 +0530

    Solution File

---
+++ Agent.py
@@ -0,0 +1,58 @@
+import numpy as np
+from collections import defaultdict
+
+class Agent:
+    def update_Q(self, Qsa, Qsa_next, reward):
+       return Qsa + (self.alpha * (reward + (self.gamma * Qsa_next) - Qsa))
+	
+    def epsilon_greedy_probs(self, Q_s, i_episode, eps=None):
+       epsilon = 1.0 / i_episode
+       if eps is not None:
+           epsilon = eps
+       policy_s = np.ones(self.nA) * epsilon / self.nA
+       policy_s[np.argmax(Q_s)] = 1 - epsilon + (epsilon / self.nA)
+       return policy_s
+    
+    def __init__(self, nA=6):
+        """ Initialize agent.
+
+        Params
+        ======
+        - nA: number of actions available to the agent
+        """
+        self.nA = nA
+        self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = 0.005
+        self.alpha = 0.3
+        self.gamma = 1.0
+        self.policy_s = None
+        self.i_episode = 1
+
+    def select_action(self,state):
+        """ Given the state, select an action.
+
+        Params
+        ======
+        - state: the current state of the environment
+
+        Returns
+        =======
+        - action: an integer, compatible with the task's action space
+        """
+        self.policy_s = self.epsilon_greedy_probs(self.Q[state],self.i_episode)
+        self.i_episode += 1
+        return np.random.choice(np.arange(self.nA),p = self.policy_s)
+
+    def step(self, state, action, reward, next_state, done):
+        """ Update the agent's knowledge, using the most recently sampled tuple.
+
+        Params
+        ======
+        - state: the previous state of the environment
+        - action: the agent's previous choice of action
+        - reward: last reward received
+        - next_state: the current state of the environment
+        - done: whether the episode is complete (True or False)
+        """
+        self.Q[state][action] = self.update_Q(self.Q[state][action],np.max(self.Q[next_state]),\
+                                           reward)
